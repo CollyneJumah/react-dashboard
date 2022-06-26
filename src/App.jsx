9031ac050e78fc3c3928dc57b6e21e4d66a96c65
CollyneJumah <collynesjumah2010@gmail.com>
2022-06-26 13:29:51 +0300

    layout design

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-glass">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
